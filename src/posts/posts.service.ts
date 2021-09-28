30d65536f6c67bd723b9090eae134943661c45dc
Seryozha <seroj.ivanyan@gmail.com>
2021-09-28 10:39:41 +0400

    fixe

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePostInput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Post } from 'src/posts/entities/post.entity';
 import { Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class PostsService {
@@ -21,20 +22,39 @@ export class PostsService {
     return post;
   }
 
-  findAll() {
-    return `This action returns all posts`;
+  async findAll() {
+    return await this.postRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: number) {
+    const post = await this.postRepository.findOne(id);
+    if (!post) {
+      throw new NotFoundException(`Post #${id} not found`);
+    }
+    return post;
   }
 
-  update(id: number, updatePostInput: UpdatePostInput) {
-    return `This action updates a #${id} post`;
+  async update(id: number, updatePostInput: UpdatePostInput): Promise<Post> {
+    const post = await this.postRepository.preload({
+      id: id,
+      ...updatePostInput,
+    });
+    if (!post) {
+      throw new NotFoundException(`User #${id} not found`);
+    }
+    return this.postRepository.save(post);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async remove(id: number): Promise<Post> {
+    const user = await this.findOne(id);
+    await this.postRepository.remove(user);
+    return {
+      id: id,
+      title: '',
+      description: '',
+      userId: 0,
+      user: null,
+    };
   }
   async findPostsByUserId(userId: number): Promise<Array<Post>> {
     const posts = await this.postRepository.find({
